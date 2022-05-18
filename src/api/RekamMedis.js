084ca8797f47600c436e1ade6a92f052bff40b33
dimasharya <dimashphewe@gmail.com>
2022-05-18 22:36:08 +0700

    update 18/05/2022

---
+++ src/api/RekamMedis.js
@@ -0,0 +1,35 @@
+import { getConfig } from "../config";
+import APICALL from "./APICALL";
+import axios from "axios";
+import QueryString from "qs";
+
+//const { apiOrigin = "https://paw-online-queue-api.herokuapp.com", audience } = getConfig();
+const { apiOrigin = "http://localhost:4000", audience } = getConfig();
+
+export function getRekamMedis(userId) {
+  return APICALL.get(`/api/rekam/${userId}`).then((res) => res.data);
+}
+
+export function getLastNoRekamMedis() {
+  return APICALL.get(`/api/rekamlast`).then((res) => res.data);
+}
+
+export function setRekamMedisBaru(userId, data) {
+  const option = {
+    method: "POST",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: QueryString.stringify(data),
+    url: `${apiOrigin}/api/rekam/${userId}`,
+  };
+  return axios(option).then((res) => res.data);
+}
+
+export function editRekamMedis(userId, data) {
+  const option = {
+    method: "PUT",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: QueryString.stringify(data),
+    url: `${apiOrigin}/api/rekam/${userId}`,
+  };
+  return axios(option).then((res) => res.data);
+}
